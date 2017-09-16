965b35ceeb0239d08db57352e49c0da214066bd2
Élie Meignan <contact@eliepse.fr>
2017-09-16 12:18:47 +0200

    Added file renaming

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 gulp.task('prod', function () {
 
     gulp.src('explode_shape_layer.jsx')
+    .pipe(rename('explode_shape_layer.min.jsx'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/'))
 
