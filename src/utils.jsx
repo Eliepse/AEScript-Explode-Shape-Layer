a7452e65cb508111376f7bd2e16aa987a4275567
Eliepse <contact@eliepse.fr>
2017-09-16 18:32:14 +0200

    Add matchName listing

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,1 +1,15 @@
-function consLog(text) { if (configs.log) $.writeln(text); }
+function consLog(text) {
+    if (configs.log)
+        $.writeln(text);
+}
+
+function listMatchNames(object) {
+    consLog("LISTING ?")
+    for(var i=1; i <= object.numProperties; i++) {
+
+        var prop = object.property(i);
+        consLog(prop.matchName + '('+ prop.name +')');
+
+    }
+
+}
