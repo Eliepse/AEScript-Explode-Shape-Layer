ab113c8bf025b882b1b8f07c848c2d36dcb266c2
Élie Meignan <contact@eliepse.fr>
2017-09-16 12:25:31 +0200

    New folder organisation

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var uglify = require('gulp-uglify');
 
 gulp.task('prod', function () {
 
-    gulp.src('explode_shape_layer.jsx')
-    .pipe(rename('explode_shape_layer.min.jsx'))
+    gulp.src('src/explode_shape_layer.jsx')
+    .pipe(rename('./explode_shape_layer.min.jsx'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/'))
 
