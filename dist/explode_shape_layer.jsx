b9d63ad11cfce3b8f5a980616c3348bd4969fabe
Élie Meignan <contact@eliepse.fr>
2022-07-17 23:16:15 +0200

    Version 1.2.0

--- dist/explode_shape_layer.jsx
+++ dist/explode_shape_layer.jsx
@@ -1,19 +1,25 @@
 var configs = {
     title: 'Explode layer tool',
     debug : false,
-    log : true,
+    log : false,
     itemAmountWarning : 50,
     dryRun : false,
 };
 
 function cLog(text) {
-    if (configs.log)
-        $.writeln(text);
+    if (!configs.log) {
+        return;
+    }
+
+    $.writeln(text);
 }
 
 function cDebug(text) {
-    if (configs.debug)
-        $.writeln(text);
+    if (!configs.debug) {
+        return;
+    }
+
+    $.writeln(text);
 }
 
 function listMatchNames(object) {
@@ -161,32 +167,38 @@ function explodeLayer(layer) {
     var pb = new ProgressBar(1, contents.numProperties, 1);
     pb.start();
 
-    // Browse through contents array
-    for(var i = contents.numProperties; i > 0; i--) {
+    try {
+        // Browse through contents array
+        for(var i = contents.numProperties; i > 0; i--) {
 
-        // Get the original property
-        var _prop = contents.property(i);
-        pb.update(contents.numProperties - i)
+            // Get the original property
+            var _prop = contents.property(i);
+            pb.update(contents.numProperties - i)
 
-        // Skip the property if not enabled
-        if (!_prop.enabled) continue;
+            // Skip the property if not enabled
+            if (!_prop.enabled) continue;
 
-        // Duplicate the original layer and rename with property name
-        var new_layer = emptyDuplicateLayer(layer)
+            // Duplicate the original layer and rename with property name
+            var new_layer = emptyDuplicateLayer(layer)
 
-        new_layer.name = layer.name + ' - ' + _prop.name;
-        new_layer.enabled = false;
-        new_layer.shy = true;
+            new_layer.name = layer.name + ' - ' + _prop.name;
+            new_layer.enabled = false;
+            new_layer.shy = true;
 
-        layers.push(new_layer);
+            layers.push(new_layer);
 
-        if (!new_layer.property("Contents").canAddProperty(_prop.matchName)) continue;
+            if (!new_layer.property("Contents").canAddProperty(_prop.matchName)) continue;
 
-        var prop = new_layer.property("Contents").addProperty(_prop.matchName)
+            var prop = new_layer.property("Contents").addProperty(_prop.matchName)
 
-        copyProperties(_prop, prop, '')
+            copyProperties(_prop, prop, 0)
 
+        }
+    } catch(e) {
+        cLog("An error occured: " + e.message);
+        pb.end();
     }
+
 
     pb.end();
 
@@ -243,165 +255,122 @@ function explode() {
 }
 
 function emptyDuplicateLayer(layer) {
-
     var new_layer = layer.containingComp.layers.addShape();
 
-    copyProperty('anchorPoint', layer, new_layer);
-    copyProperty('position', layer, new_layer);
-    copyProperty('scale', layer, new_layer);
-    copyProperty('rotation', layer, new_layer);
-    copyProperty('opacity', layer, new_layer);
+    new_layer['anchorPoint'].setValue( layer['anchorPoint'].value );
+    new_layer['position'].setValue( layer['position'].value );
+    new_layer['scale'].setValue( layer['scale'].value );
+    new_layer['rotation'].setValue( layer['rotation'].value );
+    new_layer['opacity'].setValue( layer['opacity'].value );
 
     return new_layer;
-
 }
 
-function copyProperties(origin, target, prefix) {
+var treeChildPrefix = "⌞ ";
+var propertiesBlacklist = [
+    'ADBE Vector Taper StartWidthPx',
+    'ADBE Vector Taper EndWidthPx',
+    'ADBE Vector Taper Wave Cycles',
+    'ADBE Vector Stroke Dashes',
+    'ADBE Vector Materials Group',
+];
 
-    for(var i=1; i <= origin.numProperties; i++) {
+/**
+ * Copy properties of a layer to another (recursive).
+ * 
+ * @param {Object} origin 
+ * @param {Object} target 
+ * @param {Number} level 
+ */
+function copyProperties(origin, target, level) {
+    var indent = repeatStr("  ", level);
 
-        var _prop = origin.property(i);
-
-        if(!_prop.enabled || !target.canAddProperty(_prop.matchName)) return;
-
-        cDebug(prefix + _prop.matchName);
-
-        var prop = target.addProperty(_prop.matchName);
-
-        switch (_prop.matchName) {
-
-            case 'ADBE Vector Filter - Merge':
-            copyProperty('mode', _prop, prop)
-            break;
-
-            case 'ADBE Vector Materials Group':
-            cDebug(prefix + '-- skipped');
-            break;
-
-            case 'ADBE Vector Graphic - Stroke':
-            copyPropertyStroke(_prop, prop);
-            break;
-
-            case 'ADBE Vector Graphic - Fill':
-            copyPropertyFill(_prop, prop);
-            break;
-
-            case 'ADBE Vector Transform Group':
-            copyPropertyTransform(_prop, prop);
-            break;
-
-            case 'ADBE Vector Shape - Rect':
-            copyPropertyRect(_prop, prop);
-            break;
-
-            case 'ADBE Vector Shape - Ellipse':
-            copyPropertyEllipse(_prop, prop);
-            break;
-
-            case 'ADBE Vector Shape - Star':
-            copyPropertyStar(_prop, prop);
-            break;
-
-            case 'ADBE Root Vectors Group':
-            case 'ADBE Vectors Group':
-            case 'ADBE Vector Group':
-            copyProperties(_prop, prop, prefix += '    ')
-            break;
-
-            case 'ADBE Vector Shape - Group':
-            copyPropertyShape(_prop, prop);
-            break;
-
-            case 'ADBE Vector Blend Mode':
-            prop.setValue( _prop.value );
-            break;
-
-        }
-
-    }
-
-}
-
-function copyProperty(name, origin, target) {
-    target[name].setValue( origin[name].value );
-}
-
-function copyPropertyShape(origin, target) {
-    target.property('ADBE Vector Shape').setValue( origin.property('ADBE Vector Shape').value );
-}
-
-function copyPropertyStroke(origin, target) {
-
-    copyProperty('composite', origin, target);
-    copyProperty('color', origin, target);
-    copyProperty('strokeWidth', origin, target);
-    copyProperty('lineCap', origin, target);
-    copyProperty('lineJoin', origin, target);
-    copyProperty('miterLimit', origin, target);
-
-    // TOFIX : dash are present, no mater if deleted or not ! (disabled for now)
-    // if(false && origin.dash.enabled) {
-    //
-    //     for(var i=1; i <= origin.dash.numProperties; i++) {
-    //
-    //         var dashProp = origin.dash.property(i);
-    //
-    //         if(dashProp.enabled)
-    //             target.dash.addProperty(dashProp.matchName).setValue(dashProp.value);
-    //
-    //     }
-    //
+    // if(level === 0) {
+        cDebug(indent + origin.name);
     // }
 
+    // Process (copy) each property of the origin to the target
+    for(var i=1; i <= origin.numProperties; i++) {
+        var originalProp = origin.property(i);
+        var matchName = originalProp.matchName;
+
+        // Do not copy disabled properties
+        if(!originalProp.enabled) {
+            cDebug(indent + treeChildPrefix + matchName + " (skipped: disabled)");
+            continue;
+        };
+        
+
+        // Get or create the target property
+        var targetProp = target.property(matchName);
+
+        if(!targetProp) {
+            targetProp = target.addProperty(matchName);
+        }
+        
+        // Still no property ? Then the property is probably not supported
+        if(!targetProp) {
+            cDebug(indent + treeChildPrefix + matchName + " (skipped: cannot be added)");
+            continue;
+        }
+
+        // Skip properties that cannot be set without being displayed
+        if(arrayIncludes(propertiesBlacklist, matchName)) {
+            cDebug(indent + "⚬ " + matchName + " (skipped: not editable when hidden)");
+            continue;
+        }
+        
+        // Handle property
+        if(typeof originalProp.setValue === "function") {
+            cDebug(indent + treeChildPrefix + matchName);
+            targetProp.setValue(originalProp.value);
+            continue;
+        }
+        
+        // Handle property groups/layers
+        if(originalProp.numProperties > 0) {
+            cDebug(indent + treeChildPrefix + matchName);
+            copyProperties(originalProp, targetProp, level + 1);
+            continue;
+        }
+
+        cDebug(indent + "⦵ " + matchName + " (skipped: no reason)");
+    }
 }
 
-function copyPropertyFill(origin, target) {
+/**
+ * Check if an element (the needle) is present in a given array.
+ * 
+ * @param {Array} arr 
+ * @param {*} needle 
+ * @returns Boolean
+ */
+function arrayIncludes(arr, needle) {
+    for(var i=arr.length - 1; i>=0; i--){
+        if(arr[i] === needle) {
+            return true;
+        }
+    }
 
-    copyProperty('composite', origin, target);
-    copyProperty('fillRule', origin, target);
-    copyProperty('color', origin, target);
-
+    return false;
 }
 
-function copyPropertyTransform(origin, target) {
-
-    copyProperty('anchorPoint', origin, target);
-    copyProperty('position', origin, target);
-    copyProperty('scale', origin, target);
-    copyProperty('skew', origin, target);
-    copyProperty('skewAxis', origin, target);
-    copyProperty('rotation', origin, target);
-    copyProperty('opacity', origin, target);
-
-}
-
-function copyPropertyRect(origin, target) {
-    copyProperty('shapeDirection', origin, target)
-    copyProperty('size', origin, target)
-    copyProperty('position', origin, target)
-    copyProperty('roundness', origin, target)
-}
-
-function copyPropertyEllipse(origin, target) {
-    copyProperty('shapeDirection', origin, target)
-    copyProperty('size', origin, target)
-    copyProperty('position', origin, target)
-}
-
-function copyPropertyStar(origin, target) {
-    copyProperty('shapeDirection', origin, target)
-    copyProperty('type', origin, target)
-    copyProperty('points', origin, target)
-    copyProperty('position', origin, target)
-    copyProperty('rotation', origin, target)
-    copyProperty('innerRadius', origin, target)
-    copyProperty('outerRadius', origin, target)
-    copyProperty('innerRoundness', origin, target)
-    copyProperty('outerRoundness', origin, target)
+/**
+ * Repeat a string a given times into a single string output.
+ * 
+ * @param {String} str 
+ * @param {Number} amount 
+ * @returns String
+ */
+function repeatStr(str, amount) {
+    var rtn = "";
+    for(var i=0; i<amount; i++) {
+        rtn += str;
+    }
+    return rtn;
 }
 
 function createUI(that) {
-
     if(that instanceof Panel) {
 
         var _panel = that;
@@ -411,13 +380,13 @@ function createUI(that) {
         var _panel = new Window('palette', configs.title, undefined, {
             resizeable : true,
         });
+
         _panel.show();
 
     }
 
     var btn = _panel.add("button", [10, 10, 100, 30], "Explode layer");
 
-    // _panel.text = configs.title;
     _panel.bounds.width = 120;
     _panel.bounds.height = 40;
 
